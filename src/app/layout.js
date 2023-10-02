8e15e75a11549ce545304952003634c3513ea5eb
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2023-10-02 02:32:21 +0100

    ✨ start of something stupid

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,23 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
+
+const font = localFont({
+  src: [
+    {
+      path: '/ModernEraRegular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '/ModernEraBold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +26,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={`${font.className} flex flex-col items-center justify-between min-h-screen`}>
+        <main className='grid w-full h-[100vh] md:h-full items-center justify-between bg-white md:w-[22vw] md:min-w-[380px] md:min-h-[85vh] md:m-auto md:rounded-2xl md:border-8 border-slate-900'>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
