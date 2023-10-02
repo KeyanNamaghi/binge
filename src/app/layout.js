3b15bae423e57c83b22545ffaf6fae4c93250a8b
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2023-10-02 20:57:21 +0100

    how am I so bad at css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
 import './globals.css'
-// import { Inter } from 'next/font/google'
 import localFont from 'next/font/local'
-
-// const inter = Inter({ subsets: ['latin'] })
 
 const font = localFont({
   src: [
@@ -27,7 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`${font.className} flex flex-col items-center justify-between min-h-screen bg-black`}>
+      <body className={`${font.className} flex flex-col items-center justify-between min-h-screen`}>
         <main className='grid w-full h-[100dvh] md:h-full items-center justify-between overflow-hidden bg-white md:w-[22vw] md:min-w-[380px] md:min-h-[85vh] md:m-auto md:rounded-2xl border-0 md:border-8 border-slate-900'>
           {children}
         </main>
